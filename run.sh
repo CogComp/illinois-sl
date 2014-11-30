01f4c4296946e971f816145f3b9887ed7a7c8acb
Shyam Upadhyay <shyamupa@gmail.com>
2014-11-29 20:19:41 -0600

    save

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ for item in `ls target/dependency`; do
     LOCALCLASSPATH=target/dependency/$item:$LOCALCLASSPATH
 done
 
-echo $LOCALCLASSPATH
+#echo $LOCALCLASSPATH
 
 #nice $JAVA  -ea -Xmx2096M -cp target/classes:$LOCALCLASSPATH edu.illinois.cs.cogcomp.sl.SVMLightSeq.MainClass  $*
 nice $JAVA  -ea -Xmx2096M -cp target/classes:$LOCALCLASSPATH edu.illinois.cs.cogcomp.sl.applications.tutorial.MainClass  $*
