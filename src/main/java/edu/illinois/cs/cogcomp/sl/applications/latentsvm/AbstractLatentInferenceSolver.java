1f07af0e41d79059d976850ad5c8aeff87e8dd3b
Shyam Upadhyay <shyamupa@gmail.com>
2015-01-24 11:28:23 -0600

    removed dead code copied from vivek's project

--- src/main/java/edu/illinois/cs/cogcomp/sl/applications/latentsvm/AbstractLatentInferenceSolver.java
+++ src/main/java/edu/illinois/cs/cogcomp/sl/applications/latentsvm/AbstractLatentInferenceSolver.java
@@ -6,8 +6,18 @@ import edu.illinois.cs.cogcomp.sl.util.WeightVector;
 
 public abstract class AbstractLatentInferenceSolver {
 
+	/***
+	 * computes best explaining latent structure
+	 * returns argmax_h w^T \phi(x_i,y_i,h)
+	 * @param weight
+	 * @param ins
+	 * @param goldStructure
+	 * @return
+	 * @throws Exception
+	 */
 	public abstract IStructure getBestLatentStructure(WeightVector weight,
 			IInstance ins, IStructure goldStructure) throws Exception;
+	
 	public abstract IStructure getLossAugmentedBestStructure(WeightVector weight,
 			IInstance ins, IStructure goldStructure) throws Exception;
 
