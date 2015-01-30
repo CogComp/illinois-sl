53d61afcf19f7dc0b8387fe8c60bbcb870b416e7
Shyam Upadhyay <shyamupa@gmail.com>
2015-01-30 11:22:34 -0600

    save

--- src/main/java/edu/illinois/cs/cogcomp/sl/applications/latentsvm/AbstractLatentInferenceSolver.java
+++ src/main/java/edu/illinois/cs/cogcomp/sl/applications/latentsvm/AbstractLatentInferenceSolver.java
@@ -7,8 +7,9 @@ import edu.illinois.cs.cogcomp.sl.util.WeightVector;
 public abstract class AbstractLatentInferenceSolver {
 
 	/***
-	 * computes best explaining latent structure
-	 * returns argmax_h w^T \phi(x_i,y_i,h)
+	 * computes best explaining latent structure (the latent variable completion
+	 * step) returns argmax_h w^T \phi(x_i,y_i,h)
+	 * 
 	 * @param weight
 	 * @param ins
 	 * @param goldStructure
@@ -17,8 +18,18 @@ public abstract class AbstractLatentInferenceSolver {
 	 */
 	public abstract IStructure getBestLatentStructure(WeightVector weight,
 			IInstance ins, IStructure goldStructure) throws Exception;
-	
-	public abstract IStructure getLossAugmentedBestStructure(WeightVector weight,
-			IInstance ins, IStructure goldStructure) throws Exception;
+
+	/***
+	 * returns argmax_(y,h) w^T \phi(x_i,y,h) + Delta(y,y_i)
+	 * 
+	 * @param weight
+	 * @param ins
+	 * @param goldStructure
+	 * @return
+	 * @throws Exception
+	 */
+	public abstract IStructure getLossAugmentedBestStructure(
+			WeightVector weight, IInstance ins, IStructure goldStructure)
+			throws Exception;
 
 }
