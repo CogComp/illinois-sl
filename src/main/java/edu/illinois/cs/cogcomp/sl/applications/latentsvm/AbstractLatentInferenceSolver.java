5ac111d6342ac0ed3d1783dc9267f5183704161f
Shyam Upadhyay <shyamupa@gmail.com>
2015-02-15 15:57:22 -0600

    saving partial state

--- src/main/java/edu/illinois/cs/cogcomp/sl/applications/latentsvm/AbstractLatentInferenceSolver.java
+++ src/main/java/edu/illinois/cs/cogcomp/sl/applications/latentsvm/AbstractLatentInferenceSolver.java
@@ -1,10 +1,16 @@
 package edu.illinois.cs.cogcomp.sl.applications.latentsvm;
 
+import edu.illinois.cs.cogcomp.sl.core.AbstractInferenceSolver;
 import edu.illinois.cs.cogcomp.sl.core.IInstance;
 import edu.illinois.cs.cogcomp.sl.core.IStructure;
 import edu.illinois.cs.cogcomp.sl.util.WeightVector;
 
-public abstract class AbstractLatentInferenceSolver {
+public abstract class AbstractLatentInferenceSolver extends AbstractInferenceSolver{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/***
 	 * computes best explaining latent structure (the latent variable completion
@@ -19,6 +25,7 @@ public abstract class AbstractLatentInferenceSolver {
 	public abstract IStructure getBestLatentStructure(WeightVector weight,
 			IInstance ins, IStructure goldStructure) throws Exception;
 
+	
 	/***
 	 * returns argmax_(y,h) w^T \phi(x_i,y,h) + Delta(y,y_i)
 	 * 
