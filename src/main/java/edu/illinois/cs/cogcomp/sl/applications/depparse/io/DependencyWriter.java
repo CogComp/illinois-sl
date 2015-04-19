a9fdf6df86abb34df918802ee18f0d17bdf980df
Shyam Upadhyay <shyamupa@gmail.com>
2015-04-18 21:03:06 -0500

    rm old code inherited from mst parser

--- src/main/java/edu/illinois/cs/cogcomp/sl/applications/depparse/io/DependencyWriter.java
+++ src/main/java/edu/illinois/cs/cogcomp/sl/applications/depparse/io/DependencyWriter.java
@@ -36,19 +36,6 @@ public abstract class DependencyWriter {
 
   protected boolean labeled = false;
 
-  public static DependencyWriter createDependencyWriter(String format, boolean labeled)
-          throws IOException {
-    if (format.equals("MST")) {
-      return new MSTWriter(labeled);
-    } else if (format.equals("CONLL")) {
-      return new CONLLWriter(labeled);
-    } else {
-      System.out.println("!!!!!!!  Not a supported format: " + format);
-      System.out.println("********* Assuming CONLL format. **********");
-      return new CONLLWriter(labeled);
-    }
-  }
-
   public void startWriting(String file) throws IOException {
     writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF8"));
   }
